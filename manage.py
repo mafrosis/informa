c07de70bc45e01d65fab533964c74c839635ae95
Matt Black <matt.black@jbadigital.com>
2012-07-09 09:07:25 +1000

    manage command 'load' to force plugin output

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from flask.ext.script import Manager
 from flask.ext.celery import install_commands as install_celery_commands
 
-from inform import app, views
+from inform import app, modules, views
 
 manager = Manager(app)
 install_celery_commands(manager)
@@ -12,6 +12,11 @@ install_celery_commands(manager)
 @manager.command
 def get():
     print views.get().data
+
+@manager.command
+def load(name):
+    if name in modules.keys():
+        modules[name].process()
 
 if __name__ == "__main__":
     manager.run()
