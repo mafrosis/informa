750525f2de2055190d6d0cf68deb8c66aedea438
mafro <github@mafro.net>
2014-05-08 12:02:18 +1000

    alert on ND-15 arriving on pccasegear

---
+++ inform/plugins/pccasegear.py
@@ -0,0 +1,23 @@
+from ..base_plugins.httpgrep import HttpGrepPlugin
+from ..alerts.zapier import ZapierWebHook
+
+from datetime import timedelta
+
+
+class InformPlugin(HttpGrepPlugin):
+    run_every = timedelta(minutes=10)
+    plugin_name = "pccasegear"
+
+    url = 'http://www.pccasegear.com/index.php?main_page=index&cPath=207_23'
+    terms = ['NH-D14', 'NH-D15']
+
+
+    def process(self):
+        data = super(InformPlugin, self).process()
+
+        if data['NH-D15'] is True:
+            alert = ZapierWebHook.prepare()
+            alert.send('ND-15 now available', subject='ND-15')
+            self.log('Zapier Webhook called')
+
+        return data
