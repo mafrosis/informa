021eb2c238759d126df77997f8a4d821fee996eb
mafro <github@mafro.net>
2014-05-08 10:36:52 +1000

    base HTTP grepping plugin

---
+++ inform/base_plugins/httpgrep.py
@@ -0,0 +1,36 @@
+from . import InformBasePlugin
+
+from datetime import timedelta
+
+import requests
+
+
+class HttpGrepPlugin(InformBasePlugin):
+    run_every = timedelta(minutes=10)
+    plugin_name = "httpgrep"
+
+    url = 'www.google.com'
+    terms = ['google']
+
+    def process(self):
+        try:
+            r = requests.get(self.url)
+        except:
+            print "Failed loading from {}".format(self.url)
+            return {}
+
+        data = {}
+
+        try:
+            for t in self.terms:
+                if t in r.text:
+                    data[t] = True
+                else:
+                    data[t] = False
+
+        except Exception as e:
+            print "Error grepping HTML: {}".format(e)
+            return {}
+
+        self.store(data)
+        return data
