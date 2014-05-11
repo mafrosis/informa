56e27baa79d0c3bc60210d022d6094fb0becac58
mafro <github@mafro.net>
2014-05-11 14:38:17 +1000

    celery config via Salt and flask.conf

--- config/salt/inform/flask.conf.py
+++ config/salt/inform/flask.conf.py
@@ -3,6 +3,13 @@
 DEBUG = {{ pillar['flask_debug'] }}
 SECRET_KEY = '{{ pillar['secret_key'] }}'
 
+# Celery config
+BROKER_URL = 'sqla+sqlite:////srv/inform/inform.sqlitedb'
+CELERY_DEFAULT_QUEUE = 'inform'
+CELERY_DEFAULT_EXCHANGE = 'inform'
+CELERY_DEFAULT_EXCHANGE_TYPE = 'direct'
+CELERY_DEFAULT_ROUTING_KEY = 'inform'
+
 {% if 'zapier_email_webhook_id' in pillar %}
 ZAPIER_EMAIL_WEBHOOK_ID = '{{ pillar['zapier_email_webhook_id'] }}'
 ZAPIER_EMAIL_HEARTBEAT = True
--- inform/celery.py
+++ inform/celery.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
 
+from . import app
+
 from celery import Celery
 
-celery = Celery(broker='sqla+sqlite:////srv/inform/inform.sqlitedb')
+celery = Celery(broker=app.config['BROKER_URL'])
+celery.config_from_object(app.config)
