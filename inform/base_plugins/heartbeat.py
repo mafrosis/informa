2f041b743ccfb9ae0f050cfe18e05bc44af8f93f
mafro <github@mafro.net>
2014-05-14 20:16:04 +1000

    only show heartbeat plugin data on alert

--- inform/base_plugins/heartbeat.py
+++ inform/base_plugins/heartbeat.py
@@ -1,7 +1,5 @@
 from . import InformBasePlugin
 
-from .. import app
-
 from datetime import timedelta
 
 import yaml
@@ -11,8 +9,8 @@ class HeartbeatPlugin(InformBasePlugin):
     run_every = timedelta(weeks=1)
 
     def process(self):
-        # dump all plugin data for a heartbeat alert
-        alert_data = {}
-        for m in app.config['plugins'].keys():
-            alert_data[m] = app.config['plugins'][m].load()
+        # dump plugin data for a heartbeat alert
+        alert_data = {
+            self.friendly_name: self.load()
+        }
         return yaml.dump(alert_data)
