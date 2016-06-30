00f350286abc55414d227cf8eb2258f5a4ae1159
Matt Black <github@mafro.net>
2016-06-30 13:23:08 +0100

    Retry failed SCGI calls to rtorrent

--- informa/plugins/rtorrent.py
+++ informa/plugins/rtorrent.py
@@ -1,6 +1,7 @@
 import datetime
 
 from .base import InformaBasePlugin
+from ..lib.decorators import retry
 from ..lib.rtorrent import RTorrent
 
 HOST = '172.18.0.1'
@@ -11,8 +12,13 @@ RTORRENT_XMLRPC = 'scgi://{}:{}'.format(HOST, PORT)
 class RTorrentPlugin(InformaBasePlugin):
     run_every = datetime.timedelta(minutes=1)
 
+    @retry(times=3, sleep=3)
     def process(self):
         rt = RTorrent(HOST, PORT)
         data = rt.get_torrents()
+
+        if data is None:
+            raise Exception
+
         self.store(data)
         return data
