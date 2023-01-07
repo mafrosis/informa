2c4ac4eda11aebe7e8bdc8b897108351e7eba8f4
Matt Black <github@mafro.net>
2023-01-07 16:58:06 +1100

    Support underscores as dashes in plugin name

--- informa/main.py
+++ informa/main.py
@@ -26,8 +26,11 @@ def init_plugins() -> Dict[str, ModuleType]:
     modules = {}
 
     for plug in plugins:
+        # Convert dashes into underscores for python imports
+        module_name = plug.replace('-', '_')
+
         # Dynamic import to register rocketry tasks
         logger.debug('Initialising plugin %s', plug)
-        modules[plug] = importlib.import_module(f'informa.plugins.{plug}')
+        modules[plug] = importlib.import_module(f'informa.plugins.{module_name}')
 
     return modules
