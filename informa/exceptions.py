ab5fde7cf35f78821eff026671e1aa0ac55e4397
Matt Black <github@mafro.net>
2023-04-02 06:05:50 +1000

    Better handling for plugin names from CLI

--- informa/exceptions.py
+++ informa/exceptions.py
@@ -2,6 +2,9 @@ class AppError(Exception):
     def __str__(self):
         return str(self.__doc__)
 
+class PluginNotFound(AppError):
+    'Supplied plugin name cannot be found!'
+
 class MailgunKeyMissing(AppError):
     'Environment var MAILGUN_KEY is missing. Are you running in DEBUG?'
 
