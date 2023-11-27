f94b41da0ad62fabadefc20206c5cbc9e90ff0f5
Matt Black <github@mafro.net>
2023-11-27 15:19:27 +1100

    Add fastapi with uvicorn

--- informa/main.py
+++ informa/main.py
@@ -4,18 +4,15 @@ import logging
 from types import ModuleType
 from typing import Dict
 
+import asyncio
+import uvicorn
 import yaml
 
-from informa.lib import app
+from informa.api import app_fastapi
+from informa.lib import app as app_rocketry
 
 
 logger = logging.getLogger('informa')
-
-
-def start_rocketry():
-    init_plugins()
-    logger.info('Starting rocketry workers')
-    app.run()
 
 
 @functools.lru_cache()
@@ -35,3 +32,26 @@ def init_plugins() -> Dict[str, ModuleType]:
         modules[plug] = importlib.import_module(f'informa.plugins.{module_name}')
 
     return modules
+
+
+class Server(uvicorn.Server):
+    '''
+    Customized uvicorn.Server
+
+    Uvicorn server overrides signals and we need to include Rocketry to the signals.
+    '''
+    def handle_exit(self, sig: int, frame) -> None:
+        app_rocketry.session.shut_down()
+        return super().handle_exit(sig, frame)
+
+
+async def start(host: str, port: int):
+    'Run Rocketry and FastAPI'
+    server = Server(config=uvicorn.Config(app_fastapi, loop='asyncio', host=host, port=port))
+
+    init_plugins()
+
+    await asyncio.wait([
+        asyncio.create_task(server.serve()),
+        asyncio.create_task(app_rocketry.serve()),
+    ])
