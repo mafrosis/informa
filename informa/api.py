4248b614121f57c805002927fef968d441f99bfe
Matt Black <github@mafro.net>
2024-03-09 12:44:26 +1100

    Add API /mp3/info endpoint

--- informa/api.py
+++ informa/api.py
@@ -1,7 +1,13 @@
+import os
+import pathlib
+
 from fastapi import APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 
 from informa.lib import fastapi as app_fastapi, app as app_rocketry
+
+
+MP3HOME=os.environ.get('MP3PATH', '/home/mafro/music/')
 
 
 # CORS support for React frontends
@@ -41,3 +47,30 @@ async def patch_session_config(values: dict):
 
 
 app_fastapi.include_router(router_config)
+
+
+######
+# MP3 album info API
+#
+router_mp3info = APIRouter(prefix='/mp3', tags=['mp3info'])
+
+@router_mp3info.get('/info/{identifier}')
+async def get_mp3_album_info(identifier: str):
+    def count_mp3s(dirpath: str):
+        return sum(1 for f in os.listdir(dirpath) if f.endswith('mp3'))
+
+    def find_album_path(albumartist: str):
+        for path in pathlib.Path(MP3HOME).glob(f'**/{albumartist}'):
+            if path.is_dir():
+                return str(path)
+
+    path = find_album_path(identifier)
+
+    return {
+        'identifier': identifier,
+        'path': path,
+        'track_count': count_mp3s(path),
+    }
+
+
+app_fastapi.include_router(router_mp3info)
