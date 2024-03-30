e9548a8c1388860810e58770327be32a643c460f
Matt Black <github@mafro.net>
2024-03-30 15:32:44 +1100

    Helper for pretty printing dataframe as table

--- informa/lib/pretty.py
+++ informa/lib/pretty.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 
+import pandas as pd
 from rich.console import Console
 from rich.table import Table
 
@@ -11,9 +12,21 @@ def table(data: List[dict], columns: List[str], title: Optional[str]=None):
         tbl.add_column(c.title())
 
     for item in data:
-        row = []
-        for c in columns:
-            row.append(item[c])
-        tbl.add_row(*row)
+        tbl.add_row(*[item[c] for c in columns])
+
+    Console().print(tbl)
+
+
+def dataframe(df: pd.DataFrame, title: Optional[str]=None):
+    tbl = Table(title=title)
+
+    if 'date' in df.columns:
+        df['date'] = df['date'].dt.strftime('%d-%m-%Y')
+
+    for c in df.columns:
+        tbl.add_column(c.title())
+
+    for _, item in df.iterrows():
+        tbl.add_row(*[str(s) for s in list(item)])
 
     Console().print(tbl)
