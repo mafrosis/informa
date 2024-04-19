e9357980570c45c3b5861c97670504b654f70da1
Matt Black <github@mafro.net>
2024-04-20 06:40:36 +1000

    Refactor plugin utility functions into lib.plugin module

--- informa/lib/__init__.py
+++ informa/lib/__init__.py
@@ -3,18 +3,14 @@ import datetime
 import inspect
 import logging
 import os
-from collections.abc import Callable
 from dataclasses import dataclass
 from typing import cast
 
 import yaml
 from dataclasses_json import DataClassJsonMixin
 from fastapi import FastAPI
-from marshmallow.exceptions import ValidationError
 from rocketry import Rocketry
 from zoneinfo import ZoneInfo
-
-from informa.exceptions import AppError
 
 app = Rocketry(
     config={
@@ -58,77 +54,9 @@ def pass_plugin_name(func):
     return inner
 
 
-@pass_plugin_name
-def load_run_persist(
-    plugin_name: str, logger: logging.Logger | logging.LoggerAdapter, state_cls: type[StateBase], main_func: Callable
-):
-    """
-    Load plugin state, run plugin main function via callback, persist state to disk.
-
-    Dynamically inspects the parameter `main_func` to determine if it has a parameter derived from
-    `ConfigBase`, and if so, loads a plugins config into an instance of this `ConfigBase` class.
-
-    Params:
-        plugin_name:  Plugin module name, supplied by the @pass_plugin_name decorator
-        logger:       Logger for the calling plugin
-        state_cls:    Plugin state dataclass which is persisted between runs
-        main_func:    Callback function to trigger plugin logic
-    """
-    try:
-        # Reload config each time plugin runs
-        state = _load_state(plugin_name, logger, state_cls)
-
-        plugin_config_class: type[ConfigBase] | None = None
-
-        # Introspect the passed main_func for a Config parameter
-        for param in inspect.getfullargspec(main_func).annotations.values():
-            if issubclass(param, ConfigBase):
-                plugin_config_class = param
-
-        logger.info('Running, last run: %s', state.last_run or 'Never')
-
-        if plugin_config_class:
-            # Reload config each time plugin runs
-            config = _load_config(plugin_name, plugin_config_class)
-
-            # Call plugin with config
-            main_func(state, config)
-        else:
-            # Call plugin without config
-            main_func(state)
-
-        state.last_run = now_aest()
-
-        # Write plugin state back to disk
-        _write_state(plugin_name, state)
-        logger.debug('State persisted')
-
-    except AppError as e:
-        logger.error(str(e))
-    except ValidationError as e:
-        logger.error('State ValidationError: %s', str(e))
-    except Exception:
-        logger.exception('Unhandled exception')
-
-
 def now_aest() -> datetime.datetime:
     "Utility function to return now as TZ-aware datetime"
     return datetime.datetime.now(ZoneInfo('Australia/Melbourne'))
-
-
-@pass_plugin_name
-def load_config(
-    plugin_name: str,
-    config_cls: type[ConfigBase],
-) -> DataClassJsonMixin | None:
-    """
-    Load plugin config
-
-    Params:
-        plugin_name:  Plugin module name, supplied by the @pass_plugin_name decorator
-        cls:          Plugin's config class type
-    """
-    return _load_config(plugin_name, config_cls)
 
 
 def _load_config(
@@ -137,24 +65,8 @@ def _load_config(
 ) -> DataClassJsonMixin | None:
     """
     Load plugin config
-    Doesn't use @pass_plugin_name magic
     """
     return cast(ConfigBase, _load_file(plugin_name, 'config', config_cls))
-
-
-@pass_plugin_name
-def load_state(
-    plugin_name: str, logger: logging.Logger | logging.LoggerAdapter, state_cls: type[StateBase]
-) -> StateBase:
-    """
-    Load or initialise plugin state
-
-    Params:
-        plugin_name:  Plugin module name, provided by the @pass_plugin_name decorator
-        directory:    Directory path; either "config" or "state"
-        cls:          Plugin's state class type
-    """
-    return _load_state(plugin_name, logger, state_cls)
 
 
 def _load_state(
@@ -162,7 +74,6 @@ def _load_state(
 ) -> StateBase:
     """
     Load or initialise plugin state
-    Doesn't use @pass_plugin_name magic
     """
     state = _load_file(plugin_name, 'state', state_cls)
     if not state:
@@ -193,22 +104,9 @@ def _load_file(plugin_name: str, directory: str, cls: type[DataClassJsonMixin]) 
         return None
 
 
-@pass_plugin_name
-def write_state(plugin_name: str, state_obj: StateBase):
-    """
-    Utility function to write state to a file
-
-    Params:
-        plugin_name:  Plugin module name, provided by the @pass_plugin_name decorator
-        state_obj:    Plugin's state object
-    """
-    _write_state(plugin_name, state_obj)
-
-
 def _write_state(plugin_name: str, state_obj: StateBase):
     """
     Utility function to write state to a file
-    Doesn't use @pass_plugin_name magic
     """
     if not os.path.exists('state'):
         os.mkdir('state')
--- informa/lib/plugin.py
+++ informa/lib/plugin.py
@@ -1,12 +1,26 @@
 import inspect
 import logging
+from collections.abc import Callable
 from dataclasses import dataclass
 from types import ModuleType
 
 import arrow
 import click
+import yaml
+from dataclasses_json import DataClassJsonMixin
+from marshmallow.exceptions import ValidationError
 
-from informa.lib import ConfigBase, PluginAdapter, StateBase, _load_state
+from informa.exceptions import AppError
+from informa.lib import (
+    ConfigBase,
+    PluginAdapter,
+    StateBase,
+    _load_config,
+    _load_state,
+    _write_state,
+    now_aest,
+    pass_plugin_name,
+)
 
 
 @dataclass
@@ -44,6 +58,114 @@ def setup_plugin_cli(plugin: ModuleType):
             'obj': Plugin(plugin.__name__, plugin_logger, plugin_state_cls, plugin_config_cls)
         }
         plugin.cli.add_command(plugin_last_run)
+
+
+@pass_plugin_name
+def load_run_persist(
+    plugin_name: str, logger: logging.Logger | logging.LoggerAdapter, state_cls: type[StateBase], main_func: Callable
+):
+    """
+    Load plugin state, run plugin main function via callback, persist state to disk.
+
+    Dynamically inspects the parameter `main_func` to determine if it has a parameter derived from
+    `ConfigBase`, and if so, loads a plugins config into an instance of this `ConfigBase` class.
+
+    Params:
+        plugin_name:  Plugin module name, supplied by the @pass_plugin_name decorator
+        logger:       Logger for the calling plugin
+        state_cls:    Plugin state dataclass which is persisted between runs
+        main_func:    Callback function to trigger plugin logic
+    """
+    try:
+        # Reload config each time plugin runs
+        state = _load_state(plugin_name, logger, state_cls)
+
+        plugin_config_class: type[ConfigBase] | None = None
+
+        # Introspect the passed main_func for a Config parameter
+        for param in inspect.getfullargspec(main_func).annotations.values():
+            if issubclass(param, ConfigBase):
+                plugin_config_class = param
+
+        logger.info('Running, last run: %s', state.last_run or 'Never')
+
+        if plugin_config_class:
+            # Reload config each time plugin runs
+            config = _load_config(plugin_name, plugin_config_class)
+
+            # Call plugin with config
+            main_func(state, config)
+        else:
+            # Call plugin without config
+            main_func(state)
+
+        state.last_run = now_aest()
+
+        # Write plugin state back to disk
+        _write_state(plugin_name, state)
+        logger.debug('State persisted')
+
+    except AppError as e:
+        logger.error(str(e))
+    except ValidationError as e:
+        logger.error('State ValidationError: %s', str(e))
+    except Exception:
+        logger.exception('Unhandled exception')
+
+
+@pass_plugin_name
+def write_config(plugin_name: str, config: DataClassJsonMixin):
+    """
+    Write a config file
+
+    Params:
+        plugin_name:  Plugin module name, provided by the @pass_plugin_name decorator
+        config:       Plugin config to serialise into JSON
+    """
+    with open(f'config/{plugin_name}.yaml', 'w', encoding='utf8') as f:
+        f.write(yaml.dump(config.to_dict()))
+
+
+@pass_plugin_name
+def load_config(
+    plugin_name: str,
+    config_cls: type[ConfigBase],
+) -> DataClassJsonMixin | None:
+    """
+    Load plugin config
+
+    Params:
+        plugin_name:  Plugin module name, supplied by the @pass_plugin_name decorator
+        cls:          Plugin's config class type
+    """
+    return _load_config(plugin_name, config_cls)
+
+
+@pass_plugin_name
+def load_state(
+    plugin_name: str, logger: logging.Logger | logging.LoggerAdapter, state_cls: type[StateBase]
+) -> StateBase:
+    """
+    Load or initialise plugin state
+
+    Params:
+        plugin_name:  Plugin module name, provided by the @pass_plugin_name decorator
+        directory:    Directory path; either "config" or "state"
+        cls:          Plugin's state class type
+    """
+    return _load_state(plugin_name, logger, state_cls)
+
+
+@pass_plugin_name
+def write_state(plugin_name: str, state_obj: StateBase):
+    """
+    Utility function to write state to a file
+
+    Params:
+        plugin_name:  Plugin module name, provided by the @pass_plugin_name decorator
+        state_obj:    Plugin's state object
+    """
+    _write_state(plugin_name, state_obj)
 
 
 @click.command('last-run')
