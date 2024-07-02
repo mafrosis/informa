363fa1c74840ad066c6a1f8f40c6ba8cca62c0d0
Matt Black <github@mafro.net>
2024-07-02 20:56:36 +1000

    f1torrents: Stop tracking latest race

--- informa/plugins/f1torrents.py
+++ informa/plugins/f1torrents.py
@@ -46,7 +46,6 @@ class Download(DataClassJsonMixin):
 
 @dataclass
 class State(StateBase):
-    latest_race: str = field(default='')
     races: dict[str, Download] = field(default_factory=dict)
 
 
@@ -199,7 +198,6 @@ def check_torrentgalaxy(current_season: int, state: State) -> bool:
 
     feed = feedparser.parse(resp.text)
 
-    logger.info('Latest race: %s', state.latest_race)
     ret = False
 
     RACE_TYPES = {'Race', 'Qualifying', 'Sprint', 'Season.Review', 'Shootout'}
@@ -239,9 +237,6 @@ def check_torrentgalaxy(current_season: int, state: State) -> bool:
             if key not in state.races:
                 state.races[key] = Download(key=key, title=title, magnet=magnet)
                 ret = True
-
-            if key > state.latest_race:
-                state.latest_race = key
 
     return ret
 
