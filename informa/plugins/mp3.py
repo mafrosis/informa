c3e711f7fec40f72ed984f770b6c5880d88e570f
Matt Black <github@mafro.net>
2024-07-29 14:30:39 +1000

    mp3: Also handle direct file path and base64 encoding

--- informa/plugins/mp3.py
+++ informa/plugins/mp3.py
@@ -1,6 +1,9 @@
+import base64
+import binascii
+import contextlib
 import logging
 import os
-import pathlib
+from pathlib import Path
 
 from fastapi import APIRouter
 
@@ -10,29 +13,41 @@ from informa.lib import fastapi as app_fastapi
 logger = PluginAdapter(logging.getLogger('informa'))
 
 
-MP3HOME = os.environ.get('MP3HOME', '/home/mafro/music/')
+MP3HOME = Path(os.environ.get('MP3HOME', '/home/mafro/music/'))
 
 
 router = APIRouter(prefix='/mp3')
 
 
-@router.get('/info/{identifier}')
-def get_mp3_album_info(identifier: str):
-    def count_mp3s(dirpath: str):
-        return sum(1 for f in os.listdir(dirpath) if f.endswith('mp3'))
+def find_album_path(query: str) -> Path:
+    logger.debug('Lookup: %s', query)
 
-    def find_album_path(albumartist: str):
-        for path in pathlib.Path(MP3HOME).glob(f'**/{albumartist}'):
-            if path.is_dir():
-                return str(path)
+    if (MP3HOME / Path(query)).is_file():
+        # If query is an MP3 file, then return path to directory
+        return (MP3HOME / Path(query)).parent
 
-    logger.debug('Lookup: %s', identifier)
+    # Search for the directory
+    for path in MP3HOME.glob(f'**/{query}'):
+        if path.is_dir():
+            return path
 
-    path = find_album_path(identifier)
+
+def try_base64_decode(query: bytes) -> str:
+    with contextlib.suppress(binascii.Error):
+        query = base64.b64decode(query)
+    return query.decode('utf8').strip()
+
+
+@router.get('/info/{query}')
+def get_mp3_album_info(query: bytes):
+    path = find_album_path(try_base64_decode(query))
+
+    def count_mp3s(path: Path):
+        return sum(1 for x in list(path.glob('*.mp3')))
 
     return {
-        'identifier': identifier,
-        'path': path,
+        'identifier': path.parts[-1],
+        'path': str(path),
         'track_count': count_mp3s(path),
     }
 
