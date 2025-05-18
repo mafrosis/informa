9cb8bf678642cd6b2eed2098795646fddab855ce
Matt Black <github@mafro.net>
2025-05-18 14:34:52 +1000

    mp3: Return 200 + {} if path not found

--- informa/plugins/mp3.py
+++ informa/plugins/mp3.py
@@ -19,6 +19,7 @@ MP3HOME = Path(os.environ.get('MP3HOME', '/home/mafro/music/'))
 
 router = APIRouter(prefix='/mp3')
 
+
 @app.api(router)
 def fastapi():
     # Register the APIRouter with Informa
@@ -47,6 +48,8 @@ def try_base64_decode(query: bytes) -> str:
 @router.get('/info/{query}')
 def get_mp3_album_info(query: bytes):
     path = find_album_path(try_base64_decode(query))
+    if path is None:
+        return {}
 
     def count_mp3s(path: Path):
         return sum(1 for x in list(path.glob('*.mp3')))
