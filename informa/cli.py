8e1e1170cbd13314d088214e9b6174e1a52a1c35
Matt Black <github@mafro.net>
2025-09-02 06:14:38 +1000

    Add ability to enable/disable plugins at runtime

--- informa/cli.py
+++ informa/cli.py
@@ -61,6 +61,68 @@ def start(host: str, port: int, plugins: str | None):
 def admin_():
     'Plugin management commands'
 
+
+def verify_plugin_or_raise(plugin_name: str):
+    if not plugin_name.startswith('informa.plugins.'):
+        plugin_name = f"informa.plugins.{plugin_name.replace('-', '_')}"
+    else:
+        plugin_name = plugin_name.replace('-', '_')
+
+    if plugin_name not in app.plugins:
+        raise click.ClickException(f'Invalid plugin: {plugin_name}')
+    return plugin_name
+
+
+@admin_.command
+@click.argument('plugin_name')
+@click.option('--persist', is_flag=True, default=False)
+@click.option('--host', help='Connect to Informa at hostname', default='127.0.0.1', type=str)
+@click.option('--port', help='Connect to Informa on port', default=3000, type=int)
+def enable(plugin_name: str, persist: bool, host: str, port: int):
+    'Enable a plugin'
+    plugin_name = verify_plugin_or_raise(plugin_name)
+
+    resp: requests.Response
+
+    try:
+        resp = requests.post(f'http://{host}:{port}/admin/plugins/{plugin_name}', timeout=1, params={'persist': persist})
+        resp.raise_for_status()
+
+        print(f'Plugin {plugin_name} enabled')
+
+    except requests.exceptions.ConnectionError as e:
+        raise click.ClickException('It appears that informa is currently down') from e
+    except requests.RequestException as e:
+        if 'detail' in resp.json():
+            raise click.ClickException(resp.json()['detail']) from e
+        raise click.ClickException('Failed to enable plugin') from e
+
+
+@admin_.command
+@click.argument('plugin_name')
+@click.option('--persist', is_flag=True, default=False)
+@click.option('--host', help='Connect to Informa at hostname', default='127.0.0.1', type=str)
+@click.option('--port', help='Connect to Informa on port', default=3000, type=int)
+def disable(plugin_name: str, persist: bool, host: str, port: int):
+    'Disable a plugin'
+    plugin_name = verify_plugin_or_raise(plugin_name)
+
+    resp: requests.Response
+
+    try:
+        resp = requests.delete(f'http://{host}:{port}/admin/plugins/{plugin_name}', timeout=1, params={'persist': persist})
+        resp.raise_for_status()
+
+        print(f'Plugin {plugin_name} disabled')
+
+    except requests.exceptions.ConnectionError as e:
+        raise click.ClickException('It appears that informa is currently down') from e
+    except requests.RequestException as e:
+        if 'detail' in resp.json():
+            raise click.ClickException(resp.json()['detail']) from e
+        raise click.ClickException('Failed to disable plugin') from e
+
+
 @admin_.command('list')
 @click.option('--host', help='Connect to Informa at hostname', default='127.0.0.1', type=str)
 @click.option('--port', help='Connect to Informa on port', default=3000, type=int)
